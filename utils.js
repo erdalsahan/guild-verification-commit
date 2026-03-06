7ac183d5ea8127d327dc7a0037d6c8262b0be190
erdalsahan <148914472+erdalsahan@users.noreply.github.com>
2026-03-06 09:33:59 +0300

    Resolve null pointer exception in user service

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-06T06:33:53.642Z
-Commit: Fix CSS styling bug on mobile devices
+Timestamp: 2026-03-06T06:33:59.392Z
+Commit: Resolve null pointer exception in user service
