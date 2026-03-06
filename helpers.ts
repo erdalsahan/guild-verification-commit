c55c406e7e4ca6b8c2f44a4c18382f729a311f63
erdalsahan <148914472+erdalsahan@users.noreply.github.com>
2026-03-06 09:34:04 +0300

    Resolve timezone issues in date handling

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-06T06:32:26.776Z
-Commit: Fix broken links in documentation
+Timestamp: 2026-03-06T06:34:04.256Z
+Commit: Resolve timezone issues in date handling
