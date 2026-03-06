d86949c9cdab7a8d4f9795410c0b5a2bcf8487b9
erdalsahan <148914472+erdalsahan@users.noreply.github.com>
2026-03-06 09:32:27 +0300

    Fix broken links in documentation

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-06T06:32:16.410Z
-Commit: Implement user profile customization
+Timestamp: 2026-03-06T06:32:26.776Z
+Commit: Fix broken links in documentation
