830d36ad28ace048549f414045218eb31ad2082d
erdalsahan <148914472+erdalsahan@users.noreply.github.com>
2026-03-06 09:32:48 +0300

    Patch API endpoint error handling

--- app.js
+++ app.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-06T06:32:15.260Z
-Commit: Fix broken links in documentation
+Timestamp: 2026-03-06T06:32:48.002Z
+Commit: Patch API endpoint error handling
